bbbaf5b05caa69d883e2b554a4c7d68001f13143
Cascade AI <cascade@ai.assistant>
2026-07-07 05:44:00 +0200

    debug: fix db path in query-db script

--- scratch/query-db.js
+++ scratch/query-db.js
@@ -1,6 +1,6 @@
 const Database = require('better-sqlite3');
 const path = require('path');
-const db = new Database(path.join(__dirname, '../src/database/data.db'));
+const db = new Database(path.join(__dirname, '../database.sqlite'));
 
 console.log('--- Tables in database ---');
 const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
