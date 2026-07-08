53f71850f3bdf529249632bb7cfa017b4f9f2197
Cascade AI <cascade@ai.assistant>
2026-07-08 05:09:19 +0200

    debug: query member locations in test_esc.js

--- src/utils/test_esc.js
+++ src/utils/test_esc.js
@@ -1,2 +1,3 @@
-const filename = 'test.gif';
-console.log("Result of `attachment://\${filename}`:", `attachment://${filename}`);
+const Database = require('better-sqlite3');
+const db = new Database('/home/maison/bagbot-elite/database.sqlite');
+console.log("Member Locations:", db.prepare('SELECT * FROM member_locations').all());
