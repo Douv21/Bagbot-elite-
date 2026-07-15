18c957a1bf6ce4266fab3f352bccc8feb585456a
Cascade AI <cascade@ai.assistant>
2026-07-15 22:02:21 +0200

    chore: clean up check-shop script

--- scratch/check-shop.js
+++
@@ -1,6 +0,0 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-const db = new Database(path.join(__dirname, '../database.sqlite'));
-
-console.log("SHOP ITEMS:");
-console.log(db.prepare("SELECT * FROM shop").all());
