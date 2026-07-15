5fb82fdfb964dd01c3a739cb8a3277dbf0e81617
Cascade AI <cascade@ai.assistant>
2026-07-15 16:36:27 +0200

    chore: print couleur lines

--- scratch/ssh-connect.py
+++ scratch/ssh-connect.py
@@ -7,7 +7,7 @@ def run_ssh():
     pid, fd = pty.fork()
     if pid == 0:
         # Processus enfant
-        os.execvp('ssh', ['ssh', '-o', 'StrictHostKeyChecking=no', 'bagbot@192.168.1.37', "find /home/bagbot/Bag-bot/src/ -maxdepth 1"])
+        os.execvp('ssh', ['ssh', '-o', 'StrictHostKeyChecking=no', 'bagbot@192.168.1.37', "sed -n '12990,13190p' /home/bagbot/Bag-bot/src/bot.js"])
     else:
         # Processus parent
         password_sent = False
